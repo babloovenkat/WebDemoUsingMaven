e59cf0e2b62bd6dacd61e87df7a0555ae2cb5615
babloovenkat <43472182+babloovenkat@users.noreply.github.com>
2021-03-24 09:32:35 +0530

    Add files via upload

---
+++ Servletprograms/demoservlethttp/src/main/java/com/demo/servlet/addservlet.java
@@ -0,0 +1,49 @@
+package com.demo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class addservlet
+ */
+@WebServlet("/addservlet")
+public class addservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public addservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		int x=Integer.parseInt(request.getParameter("a"));
+		int y=Integer.parseInt(request.getParameter("b"));
+		int z=x+y;
+		PrintWriter out=response.getWriter();
+		out.println("ADDITION IS:"+z);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
